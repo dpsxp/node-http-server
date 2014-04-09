6b1427ae8ac580d3f892fec3c4d0ab2ad1d3dc40
Daniel Paulino <danielpaulino030@gmail.com>
2014-04-09 00:12:00 -0300

    Optional args, and can work from everywhere

--- server.js
+++ server.js
@@ -1,23 +1,51 @@
-var ROOT_PATH = 'app/'
-var PORT = 3000
+// Node simpe http server
+
+// Get optional args
+var args = process.argv.splice(2);
+var formattedArgs = {};
+
+// Simple args parse
+for (var i = 0, l = args.length; i < l; i ++) {
+  if (args[i].match(/^-+/)) {
+    formattedArgs[args[i]] = args[i + 1];
+  }
+};
+
+// Setup constants
+var ROOT_PATH = process.cwd() + '/';
+ROOT_PATH += formattedArgs['-r'] || formattedArgs['--root'] || '';
+ROOT_PATH += '/';
+var PORT = formattedArgs['-p'] || formattedArgs['--port'] || 3000;
+
+// Required modules
 var http = require('http');
 var fs = require('fs');
-var server = http.createServer();
+var url = require('url');
+
+var types = {
+  'js' : 'application/javascript',
+  'json': 'application/json',
+  'html' : 'text/html',
+  'png': 'image/png',
+  'jpeg': 'image/jpeg',
+  'gif': 'image/gif'
+};
 
 function getType (url) {
-  var type = url.split('/').pop().split('.')[1];
-  var contentType = 'text/html';
-  if (type === 'js') {
-    contentType = 'application/json';
+  var type = url.split('.').pop();
+  // Default extension
+  if (type === '/') {
+    type = 'html';
   }
-  return contentType;
+  return types[type];
 }
 
 function getFile (url) {
   if (url === '/') {
+    // Index path
     return ROOT_PATH + 'index.html';
   } else {
-    return ROOT_PATH + Array.prototype.splice.call(url, 1, url.length).join('');
+    return ROOT_PATH + url;
   }
 }
 
@@ -26,23 +54,29 @@ function notFound(url, res) {
   res.writeHead(404, {"Content-Type": 'text/html'});
 }
 
+var server = http.createServer();
+
 server.on('request', function(req, res) {
-  var url = req.url, type, file;
-  console.log('Serving request for ', url)
-  type = getType(url);
-  file = getFile(url);
+  var path = url.parse(req.url).pathname,
+      type,
+      file;
+
+  console.log('Serving request for ', path);
+  type = getType(path);
+  file = getFile(path);
+  console.log(file);
 
   fs.readFile(file, function (err, data) {
     if (err) {
-      notFound(url, res);
+      notFound(path, res);
     } else {
       res.writeHead(200, {"Content-Type": type});
       res.write(data);
     }
     res.end();
   });
-})
+});
 
 server.listen(PORT, function () {
-  console.log('Server started at port 3000')
+  console.log('Server started at port ' + PORT);
 });
